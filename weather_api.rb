266b2c9c64ef57852a100e92bbcd1b408c023eec
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-08 23:39:50 +0900

    Added weather

--- weather_api.rb
+++ weather_api.rb
@@ -16,22 +16,22 @@ def fetch_weather(message)
   begin
     data_serialized = open(url).read
   rescue OpenURI::HTTPError => e
-    return { mostly: '', temps: '', report: 'No weather forecast for this city...' }
+    return { mostly: '', temps: '', report: 'Preguntale a google oe :(...' }
   end
   data = JSON.parse(data_serialized)['daily'][0..3]
 
-  days = ['today', 'tomorrow', (Date.today + 2).strftime('%A'), (Date.today + 3).strftime('%A')]
+  days = ['hoy', 'mañana', (Date.today + 2).strftime('%A'), (Date.today + 3).strftime('%A')]
   weather_forcast = data.map.with_index { |day, index| [days[index], day['weather'][0]['main'], day['temp']['day'] - 272.15] }
   freq = weather_forcast.map { |day| day[1] }.inject(Hash.new(0)) { |h, v| h[v] += 1; h }
   most_freq_weather = freq.max_by { |_k, v| v }[0]
 
   # Report creation
-  report = "The weather is mostly #{most_freq_weather.upcase} in #{location} for the next 4 days.\n"
+  report = "Clima en #{location} para los siguientes 4 days, csm.\n"
   # If there are particular weather days
   other_weathers = weather_forcast.reject { |day| day[1] == most_freq_weather}
-  report += "Except on #{other_weathers.map { |day| "#{day[0]}(#{day[1]})" }.join(", ")}.\n" if other_weathers.any?
+  report += "Exepto en #{other_weathers.map { |day| "#{day[0]}(#{day[1]})" }.join(", ")}.\n" if other_weathers.any?
   # tempreatures
-  report += "\nThe temperature will be:\n#{weather_forcast.map { |day| " #{day[2].round}˚C for #{day[0]}" }.join("\n")}"
+  report += "\nTemperatura:\n#{weather_forcast.map { |day| " #{day[2].round}˚C para #{day[0]}" }.join("\n")}"
   # Return the string from fore_cast data
   return report
 end
