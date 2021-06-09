703c29648a20323b088bb6d416f61deff6ba1173
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-09 11:21:39 +0900

    Revert "Nevermind, didnt work"
    This reverts commit 0b7dee8b626e17952adb93b263fb25f5b837e917.

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ def bot_answer_to(message, user_name)
     # call weather API in weather_api.rb
     fetch_weather(message)
   elsif message.downcase.include?('reu')
-    "Para cuando pe?"
+    upcoming_events
   elsif message.downcase.include?('events')
     # call events API in tokyo_events.rb
     fetch_tokyo_events
