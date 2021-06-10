3c9622d4f03391eb7b13c92b1b93b3bf6b9829ba
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-10 15:29:55 +0900

    Oh no!

--- twitch_api.rb
+++ twitch_api.rb
@@ -1,4 +1,4 @@
 def twitch_api_status
     twitch_client = Twitch::Client.new(client_id: 'k4928r3bvo73781oqcxrrm1xuela01')
-    p client.get_users({login: "thecresptv"}).data.first
+    p twitch_client.get_users({login: "thecresptv"}).data.first
 end
