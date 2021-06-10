70740556bb005272b20f0f3bba75c913ecd27660
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-10 15:27:03 +0900

    Integrated twitch api with ruby gem

--- twitch_api.rb
+++ twitch_api.rb
@@ -1,4 +1,4 @@
 def twitch_api_status
     twitch_client = Twitch::Client.new(client_id: 'k4928r3bvo73781oqcxrrm1xuela01')
-      p twitch_client
+    p client.get_users({login: "thecresptv"}).data.first
 end
