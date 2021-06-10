5b4f124f4aa164b9194a11f75107b9d5306f50f3
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-10 15:23:00 +0900

    please work

--- twitch_api.rb
+++ twitch_api.rb
@@ -1,16 +1,4 @@
 def twitch_api_status
-    twitch_client = Twitch::Client.new(
-        client_id: 'k4928r3bvo73781oqcxrrm1xuela01',
-        client_secret: 'mfppcmgp69jzfxtj9jdgfzkqat1erc',
-      
-        ## this is default
-        # token_type: :application,
-      
-        ## this can be required by some Twitch end-points
-        # scopes: scopes,
-      
-        ## if you already have one
-        # access_token: access_token
-      )
+    twitch_client = Twitch::Client.new(client_id: 'k4928r3bvo73781oqcxrrm1xuela01')
       p twitch_client
 end
