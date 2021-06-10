2881eed5c81bea915007bc2bfeb02dbbd24e1480
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-10 15:17:45 +0900

    please work

--- twitch_api.rb
+++ twitch_api.rb
@@ -1,7 +1,6 @@
 def twitch_api_status
     twitch_client = Twitch::Client.new(
-        client_id: 'k4928r3bvo73781oqcxrrm1xuela01',
-        client_secret: "mfppcmgp69jzfxtj9jdgfzkqat1erc"
-        )
-        p twitch_client
-    end
+      client_id: 'k4928r3bvo73781oqcxrrm1xuela01',
+      client_secret: "mfppcmgp69jzfxtj9jdgfzkqat1erc")
+      p twitch_client
+end
