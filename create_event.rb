cf356262a8441f0fb2a948d8cb686f795f79f3e6
Ivan Reyes <ivanwilf@hotmail.com>
2021-06-14 17:42:06 +0900

    typo

--- create_event.rb
+++ create_event.rb
@@ -3,8 +3,7 @@ def send_bot_event(message, client, event)
   p event['replyToken']
   p client
 
-  message = [{ type: 'datetimepicker', label: "Test", data: "storeId=12345", mode: "datetime" }
-  p message
+  message = { type: 'datetimepicker', label: "Test", data: "storeId=12345", mode: "datetime" }
   client.reply_message(event['replyToken'], message)
   'OK'
 end
